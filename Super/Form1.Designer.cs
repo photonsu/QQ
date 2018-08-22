d01344d1289a57f013052ee1c5ecc8dbb5a84fa4
Tony <Tony@TTDC_SERVER>
2018-08-22 11:49:55 +0800

    First Teset

---
+++ Super/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Home18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepPink;
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(286, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "回18";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DeepPink;
+            this.button2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ForeColor = System.Drawing.Color.Yellow;
+            this.button2.Location = new System.Drawing.Point(286, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "今天誰掃地";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Super/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Home18
+{
+
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+            Console.WriteLine("這個會在輸出視窗");
+        }
+        private BraveActionClass BraveActionList = new BraveActionClass();
+        string StartMessage = "歡迎光臨18";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{StartMessage}");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BraveActionList.Back();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BraveActionList.LotteryMember();
+        }
+    }
+    public class BraveActionClass
+    {
+        public void Back()
+        {
+            MessageBox.Show("正在前往18....");
+        }
+
+        public void LotteryMember()
+        {
+            string member = "";
+
+            //實體化Random物件
+            Random random = new Random();
+            int returnInt = random.Next(1, 5);
+
+            switch (returnInt)
+            {
+                case 1:
+                    member = "SuQ";
+                    break;
+                case 2:
+                    member = "GMeow";
+                    break;
+                case 3:
+                    member = "臭到爆";
+                    break;
+                case 4:
+                    member = "舉呱";
+                    break;
+                case 5:
+                    member = "肥Q";
+                    break;
+            }
+
+            MessageBox.Show(member, "今天掃地的人是");
+        }
+    }
+}
